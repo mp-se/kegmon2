a8c858eaee1630451ecf3bc5dc4ca8968218bf24
Magnus <51722613+mp-se@users.noreply.github.com>
2026-04-19 13:28:57 +0200

    Validated tft pin settings with new pcb

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,8 @@
 void checkCoreDump();
 void dumpDataToInfluxDB();
 
-SerialDebug mySerial(115200L);
+// SerialDebug mySerial(115200L);
+SerialDebug mySerial(56700L);
 KegConfig myConfig(CFG_MDNSNAME, CFG_FILENAME);
 WifiConnection myWifi(&myConfig, CFG_APPNAME, "password", CFG_MDNSNAME);
 OtaUpdate myOta(&myConfig, CFG_APPVER);
